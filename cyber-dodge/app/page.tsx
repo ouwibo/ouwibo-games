43e50efa71070c392587898b0a6c4ffdd8aec67b
OUWIBO <ouwibo@gmail.com>
2026-02-07 17:06:55 +0000

    chore: remove pages/index to avoid App/Pages router conflict

--- cyber-dodge/app/page.tsx
+++ cyber-dodge/app/page.tsx
@@ -1,5 +1,11 @@
-import Home from './home';
+import { Metadata } from 'next';
 
-export default function Page() {
-  return <Home />;
+export const metadata: Metadata = {
+  other: {
+    'base:app_id': '6984a6d24609f1d788ad2bd3',
+  },
+};
+
+export default function Home() {
+  return (<div>{/* Your page content */}</div>);
 }
